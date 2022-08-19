bef8fcebd710952073ba40af4bb63e16f45467f8
Michael Bastos <bastosmichael@gmail.com>
2022-08-18 22:28:54 -0500

    Initial Commit

---
+++ lib/bastos/coin.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "coin/version"
+
+module Bastos
+  module Coin
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bastos/coin/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bastos
+  module Coin
+    VERSION = "0.1.0"
+  end
+end
